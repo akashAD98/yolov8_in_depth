ad5b68b662f852c87185cc16fa87284d253adc4b
Akash A Desai <62583018+akashAD98@users.noreply.github.com>
2023-06-27 14:04:15 +0530

    Update yolov8_predict.py

--- yolov8_predict.py
+++ yolov8_predict.py
@@ -1,21 +1,30 @@
 import csv
+import os
 from ultralytics import YOLO
 
 model = YOLO("yolov8n-seg.pt")
 
-# Perform object detection and get the results
-results = model.predict(source="/content/frames", conf=0.50, save=True)
+# Specify the folder path containing the images
+folder_path = "/content/frames"
+
+# Retrieve the file names in the folder
+file_names = os.listdir(folder_path)
 
 # Create a CSV file to save the results
-csv_file = open("results.csv", "w", newline="")
+csv_file = open("resultsnew.csv", "w", newline="")
 csv_writer = csv.writer(csv_file)
 csv_writer.writerow(["file name", "class name", "id", "score", "bbox", "mask"])
 
 # Access the class names, class IDs, probabilities, bounding boxes, and masks
-for i, r in enumerate(results):
-    file_name = f"img{i+1}.jpg"  # Assign a unique file name for each result
-    boxes = r.boxes
-    masks = r.masks
+for file_name in file_names:
+    image_path = os.path.join(folder_path, file_name)
+
+    # Perform object detection on the image and get the results
+    results = model.predict(source=image_path, conf=0.50)
+
+    # Retrieve the class names, class IDs, probabilities, bounding boxes, and masks
+    boxes = results[0].boxes
+    masks = results[0].masks
     class_ids = boxes.cls.cpu().numpy().astype(int).tolist()
     probabilities = boxes.conf.cpu().numpy().tolist()
 
